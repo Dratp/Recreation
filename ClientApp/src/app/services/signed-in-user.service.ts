f960a550578b1685c69f892a69be358ddd26f0b1
Dave Gedert <DaveGedert@QuickenLoans.com>
2020-10-29 16:55:01 -0400

    broken login... still

--- ClientApp/src/app/services/signed-in-user.service.ts
+++ ClientApp/src/app/services/signed-in-user.service.ts
@@ -1,30 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { UserInfo } from '../interfaces/user';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignedInUserService {
+  user: UserInfo;
   username: string;
   password: string;
+  
 
-  constructor(private http: HttpClient) {
-    this.username = '';
-  }
+  constructor(private http: HttpClient) {}
 
-  apiUrl = '/api/Login';
+  apiUrl = '/api/User/Login';
+  signIn(username, password): Observable<userInfo>{
+    this.user.response = false;
 
+    this.user = this.http.get<UserInfo>
+     (`${this.apiUrl}/{username}/{password}`)
+    if (this.user.response) {
+      return
+    } else if ()
+    {return
+    };
+}
 
-
-  signIn(user: string): void {
-    this.username = user;
-  }
-
-  validateUser(user: string, pass: string): boolean {
+validateUser(user: string, pass: string):
+boolean {
     if (user != '') {
-      this.signIn(user);
+      this.signIn(user, pass);
       return true;
     }
     return false;
   }
+}
 }
