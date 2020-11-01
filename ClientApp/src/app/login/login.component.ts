572975abc9a048ece51ab8810b46d70e859f742d
David Heizer <DavidHeizer@QuickenLoans.com>
2020-11-01 16:13:40 -0500

    Login worksgit add .git add .

--- ClientApp/src/app/login/login.component.ts
+++ ClientApp/src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { SignedInUserService } from '../services/signed-in-user.service';
 import { UserInfo } from '../interfaces/user';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -20,17 +21,17 @@ export class LoginComponent implements OnInit {
 
   login(username: string, password: string) {
     console.log(`username = ${username} password = ${password}`)
-    this.UserService.signIn(username, password).subscribe(user => this.user = user);
-    console.log(`response = ${this.user.response} name = ${this.user.userID} ${this.user.userName} reason = ${this.user.reason}`)
+    this.UserService.signIn(username, password);
     
-    if (this.user.response) {
-      this.UserService.username = this.user.userName;
-      this.UserService.userId = this.user.userID;
+    console.log(`response = ${this.user.response} name = ${this.user.userID} ${this.user.userName} reason = ${this.user.reason}`)
+
+    if (this.UserService.userResponse) {
       console.log(`${this.UserService.username} , ${this.UserService.userId}`);
       this.route.navigate([""]);
     }
     
   }
+
 
   createUser(firstNameBox: string, lastNameBox: string, cityBox: string, stateBox: string, zipBox: string, newUserNameBox: string, passBox: string) {
 
--- ClientApp/src/app/services/signed-in-user.service.ts
+++ ClientApp/src/app/services/signed-in-user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UserInfo } from '../interfaces/user';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,30 +11,33 @@ export class SignedInUserService {
   currentUser: UserInfo;
   username: string;
   userId: number;
+  userResponse: boolean;
   apiUrl = '/api/User/Login';
   
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private route: Router) {
     this.username = "";
     this.userId = 0;
-    this.refresh();
+    this.userResponse = false;
   }
 
-  refresh() {
-    this.signIn("username", "password")
+  SetUser(user: UserInfo) {
+    this.username = user.userName;
+    this.userId = user.userID;
+    this.userResponse = user.response;
+    if (user.response) {
+      this.route.navigate([""]);
+    }
   }
-  
-  signIn(username, password): Observable<UserInfo>
-  {
-    console.log("Sign in service called");
+
+  authUser(username, password): Observable<UserInfo> {
     return this.http.get<UserInfo>
       (`${this.apiUrl}/${username}/${password}`);
+  }
 
-    /*if (this.user.response) {
-      return
-    } else if (!this.user.response)
-    {return
-    };*/
+  signIn(username, password)
+  {
+    this.authUser(username, password).subscribe(results => this.SetUser(results));
   }
 
 validateUser(user: string, pass: string):
