43d8516c2186ec3376ac97efd4fe8e94bad320e3
Tyler Satkowiak <tylersatkowiak@quickenloans.com>
2020-11-03 16:24:11 -0500

    just pushing

--- ClientApp/src/app/favorites/favorites.component.ts
+++ ClientApp/src/app/favorites/favorites.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SignedInUserService } from '../services/signed-in-user.service';
-import { UserInfo } from '../interfaces/user';
+import { UserInfo, UserFavorite } from '../interfaces/user';
 import { Router } from '@angular/router';
 import { FavoriteService } from '../services/favorite.service';
 
@@ -11,7 +11,7 @@ import { FavoriteService } from '../services/favorite.service';
 })
 export class FavoritesComponent implements OnInit {
   user: UserInfo;
-
+  favorite: UserFavorite[];
 
   constructor(private UserService: SignedInUserService, private route: Router, private favorites: FavoriteService) { }
 
