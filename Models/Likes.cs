beb3e8ef6122e7b7dfe072952d0d52be1147403f
Kathryn Pidek <KathrynPidek@quickenloans.com>
2020-11-04 16:22:41 -0500

    adding changes to models

--- Models/Likes.cs
+++ Models/Likes.cs
@@ -13,13 +13,8 @@ namespace Recreation.Models
         public long ID { get; set; }
         public long UserID { get; set; }
         public long RIDBActivity { get; set; }
-        public long FacilityID { get; set; }
-        public string FacilityName { get; set; }
-        public string FacilityLatitude { get; set; }
-        public string FacilityLongitude { get; set; }
-        public string FacilityDescription { get; set; }
-    }
 
+    }
     public class SavedLikeInfo
     {
         
