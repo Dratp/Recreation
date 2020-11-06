1abeea32b7bc8b7c8117ed8c9b55df142c72f60c
Dave Gedert <DaveGedert@QuickenLoans.com>
2020-11-06 11:39:04 -0500

    working scroll and Close Window

--- Services/DAL.cs
+++ Services/DAL.cs
@@ -20,7 +20,7 @@ namespace Recreation.Services
         {
 
 
-            db = new SqlConnection(config.GetConnectionString("Tyler"));
+            db = new SqlConnection(config.GetConnectionString("Dave"));
 
         }
 
